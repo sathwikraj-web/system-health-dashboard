15763e7f44c751fa2891e61181cd7ec9fffec260
sathwikraj-web <sathwik.college@gmail.com>
2026-05-06 15:07:54 +0530

    Add Procfile and configure port for Railway deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, jsonify
 import psutil
 import datetime
+import os
 
 app = Flask(__name__)
 
@@ -37,4 +38,5 @@ def api_stats():
     return jsonify(get_system_stats())
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
